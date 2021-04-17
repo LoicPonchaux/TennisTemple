2ee18bb904af9ed1e2cd54d05362e4b4895b23a0
Loic Ponchaux <loicponchaux@yahoo.fr>
2021-04-17 11:32:21 +0200

    Class permettant de récuperer les liens

---
+++ lien.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+from bs4 import BeautifulSoup  # import de bs4.BeautifulSoup
+from urllib import request  # import de urllib.request
+
+#Class permettant la composition du lien à chercher
+
+class Lien():
+
+    def __init__(self):
+        self.date_jour = None
+        self.date_semaine = None
+        self.nb_page = 0
+        self.lien_hebdo = "https://fr.tennistemple.com/pronostics/classement/"
+        self.lien_jour = "https://fr.tennistemple.com/pronostics/classement/jour/"
+
+    def nombre_de_page (self, nb):
+        self.nb_page = nb
+
+    def get_nb_page(self):
+        return  self.nb_page
+
+    def ajout_date_jouralier (self, date):
+        self.date_jour = date
+
+    def ajout_date_semaine (self, date):
+        self.date_semaine = date
+
+    def recuperation_lien_journalier(self):
+        return self.lien_jour+str(self.date_jour)
+
+    def recuperation_lien_hebdo(self):
+        return self.lien_hebdo+str(self.date_semaine)
+
+    def recuperation_liens (self, type):
+        tab = []
+        if (type == "J"):
+            for i in range(self.nb_page):
+                tab.append(self.recuperation_lien_journalier()+"/"+str(i+1))
+        if (type == "H"):
+            for i in range(self.nb_page):
+                tab.append(self.recuperation_lien_hebdo()+"/"+str(i+1))
+        return  (tab)
